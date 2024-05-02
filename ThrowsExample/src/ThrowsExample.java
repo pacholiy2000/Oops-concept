945f8bb29a41b9d0e3e78323177d94ecef1244eb
Yashika <pacholiy2000@gmail.com>
2024-05-02 16:44:03 +0530

    message

--- ThrowsExample/src/ThrowsExample.java
+++ ThrowsExample/src/ThrowsExample.java
@@ -1,9 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream; 
+import java.io.IOException;
 
 public class ThrowsExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			findTheFile();
+		}
+catch(IOException e){
+	System.out.println("there is no such file");
+	e.printStackTrace();
+	System.out.println(e);
+	
+}
+	}
+	public static void findTheFile() throws IOException{
+		
+		File newfile=new File("yashist.txt");
+		FileInputStream stream=new FileInputStream(newfile);
 	}
 
 }
